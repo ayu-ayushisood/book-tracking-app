108090faab8970e418ef409616921ccd9b96404f
ayu-ayushisood <sood.ayushi30@gmail.com>
2021-09-12 17:18:06 +0530

    fix thumbnail

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,11 +5,12 @@ import PropTypes from 'prop-types';
 const Book = props => {
     const {book, onChangeShelf} = props;
     const authors = book.authors === undefined ? 'Unknown Author' : book.authors.join(', ');
+    const thumbnail = book.imageLinks === undefined ? `https://dummyimage.com/120x220/000/fff.png&text=No+Thumbnail` : book.imageLinks.thumbnail; 
     return (
         <li>
             <div className="book">
                 <div className="top_section">
-                    <img src={book.imageLinks.thumbnail} className="book_cover"/>
+                    <img src={thumbnail} className="book_cover"/>
                     <SelectBook book={book} onChangeShelf={onChangeShelf}/>
                 </div>
                 <div className="title">{book.title}</div>
